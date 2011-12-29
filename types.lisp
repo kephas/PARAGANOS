00ee20aceb6a4639d4df97f7622c998b30271235
Pierre THierry <pierre@nothos.net>
2011-12-29 20:22:00 +0100

    Add types compatibility tests

---
+++ types.lisp
@@ -0,0 +1,18 @@
+(in-package :thierry-technologies.com/2011/12/paraganos)
+
+(define-test typing
+  (assert-true (compatible-types? 'integer 'number))
+  (assert-true (compatible-types? '(seq 'integer) '(vector 'number)))
+  (assert-false (compatible-types? 'integer 'string))
+  (assert-false (compatible-types? '(seq 'integer) 'number))
+  (assert-false (compatible-types? '(list (seq 'integer)) '(container number))))
+
+(defun compatible-types? (from to)
+  (if (and (listp from) (listp to))
+      (compatible-types? (second from) (second to))
+      (if (or (listp from) (listp to))
+	  nil
+	  (subtypep from to))))
+
+(defun compatible-sources? (from to)
+  (compatible-types? (src-type from) (src-type to)))
